13ab62e262193628a5330687341fdb9e615680b0
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 09:43:51 +0700

    Add files via upload

---
+++ Tugas1_2.php
@@ -0,0 +1,48 @@
+<?php
+$_a = "Cireng" ;
+$_b = "Es DUren";
+$_c = "Es Milo";
+$_d = "Es Teh";
+$_e = "Piscok";
+
+$_1 = 1000 ;
+$_2 = 15000;
+$_3 = 20000;
+$_4 = 5000;
+$_5 = 1000;
+
+echo "
+<table>
+    <tr>
+        <td>No</td>
+        <td>Nama Menu</td>
+        <td>Harga</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>".$_a."</td>
+        <td>Rp ".$_1."</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>".$_b."</td>
+        <td>Rp ".$_2."</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>".$_c."</td>
+        <td>Rp. ".$_3."</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>".$_d."</td>
+        <td>Rp. ".$_4."</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>".$_e."</td>
+        <td>Rp. ".$_5."</td>
+    </tr>
+</table>
+"
+?>
